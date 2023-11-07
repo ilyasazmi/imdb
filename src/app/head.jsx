bfcc35e5c9815d24d9b5ef99661295d4a1534bf9
ilyasazmi <ilyasthemidas@yahoo.com>
2023-11-07 11:15:35 +0800

    1st commit

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDB clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="This is IMDB mafa ka" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
